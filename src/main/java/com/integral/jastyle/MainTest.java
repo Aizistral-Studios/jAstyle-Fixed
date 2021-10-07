2711de1bd2abc2cedc777359e5f3dfb0f45f0fb6
Extegral <VictorShadow-Integral@yandex.ru>
2021-10-07 20:19:36 +0200

    Update MainTest.java

--- src/main/java/com/integral/jastyle/MainTest.java
+++ src/main/java/com/integral/jastyle/MainTest.java
@@ -8,6 +8,10 @@ import java.io.StringReader;
 import java.io.StringWriter;
 import java.io.Writer;
 import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import com.github.abrarsyed.jastyle.ASFormatter;
 import com.github.abrarsyed.jastyle.constants.EnumFormatStyle;
@@ -16,7 +20,18 @@ import com.google.common.io.Files;
 
 public class MainTest {
 
+    private static int collapseColor(int color) {
+        return ~color & 15;
+    }
+    
 	public static void main(String[] args) throws Exception {
+	    int color = -15;
+	    
+	    System.out.println("Color " + color + " collapsed to " + collapseColor(color));
+	    
+	    if (true)
+	        return;
+	    
 		File testClass = new File("C:\\Users\\Vessel of Darkness\\Desktop\\RandomClass.java");
 		FileInputStream testInput = new FileInputStream(testClass);
 		String testClassContent = new String(ByteStreams.toByteArray(testInput), Charset.defaultCharset());
